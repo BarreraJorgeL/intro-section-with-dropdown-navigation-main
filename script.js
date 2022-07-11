cc49a35fa9cd903eb9c28a7cbfd572b683f57418
Jorge Barrera <Barrera.brjl@gmail.com>
2022-07-10 23:54:12 -0500

    now it has mouse events

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const header = document.querySelector('.js-header');
 const menu = document.querySelector('.js-menu');
+const subMenus = Array.from(document.querySelectorAll('[data-name = "toggleSubmenu"]'));
 const body = document.querySelector('.js-body');
 const bodyState = document.querySelector('[data-body-state]');
 
-/*
-function placeMenu(){
-    let menuWidth = menu.getBoundingClientRect().width;
-    let pageWidth = body.getBoundingClientRect().width;
-    console.log((menuWidth/pageWidth)*100);
-    menu.style.right = `-${(menuWidth/pageWidth)*100}%`;
-}
-
-placeMenu();
-*/
 function toggleMenu(menu){
     switch(menu.dataset.menuState){
         case 'closed':
@@ -28,24 +19,40 @@ function toggleMenu(menu){
     }
 }
 
-function toggleSubmenu(submenu){
+function toggleSubmenu(target, submenu){
     switch(submenu.dataset.submenuState){
         case 'closed':
             submenu.style.height = 'auto';
             submenu.dataset.submenuState = 'open';
+            target.firstElementChild.src = 'images/icon-arrow-up.svg';
             break;
         case 'open':
             submenu.style.height = '0';
             submenu.dataset.submenuState = 'closed';
+            target.firstElementChild.src = 'images/icon-arrow-down.svg';
             break;
     }
 }
 
-header.addEventListener('click', e => {
+document.addEventListener('click', e => {
     if(e.target.dataset.name == 'toggleMenu'){
         toggleMenu(menu);
     }
-    else if(e.target.dataset.name == 'toggleSubmenu'){
-        toggleSubmenu(e.target.nextElementSibling);
+    else if(e.target.dataset.name == 'toggleSubmenu' && window.innerWidth <= 900){
+        toggleSubmenu(e.target, e.target.nextElementSibling);
     }
 });
+
+
+subMenus.forEach(subMenu => {
+    subMenu.offsetParent.addEventListener('mouseenter', () => {
+        if(window.innerWidth >= 900){
+            toggleSubmenu(subMenu.offsetParent.firstElementChild, subMenu.offsetParent.lastElementChild);
+        }
+    })
+    subMenu.offsetParent.addEventListener('mouseleave', () => {
+        if(window.innerWidth >= 900){
+            toggleSubmenu(subMenu.offsetParent.firstElementChild, subMenu.offsetParent.lastElementChild);
+        }
+    })
+})
