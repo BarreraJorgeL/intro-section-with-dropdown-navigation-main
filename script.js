9a6b03f56760ebacb27b0c7db3cc21245793e898
Jorge Barrera <Barrera.brjl@gmail.com>
2022-07-17 14:47:10 -0500

    mavigation bar bug fixed

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-const header = document.querySelector('.js-header');
 const menu = document.querySelector('.js-menu');
-const subMenus = Array.from(document.querySelectorAll('[data-name = "toggleSubmenu"]'));
+const subMenus = Array.from(document.querySelectorAll('.js-submenu'));
 const body = document.querySelector('.js-body');
-const bodyState = document.querySelector('[data-body-state]');
 
 function toggleMenu(menu){
     switch(menu.dataset.menuState){
         case 'closed':
             menu.style.transform = 'translateX(-100%)';
             menu.dataset.menuState = 'open';
-            bodyState.dataset.bodyState = 'disable';
+            body.dataset.bodyState = 'disable';
             break;
         case 'open':
             menu.style.transform = 'translateX(0)';
             menu.dataset.menuState = 'closed';
-            bodyState.dataset.bodyState = 'enable';
+            body.dataset.bodyState = 'enable';
             break;
     }
 }
+function closeSubmenu(submenuButton, submenu){
+    submenu.style.height = '0';
+    submenu.dataset.submenuState = 'closed';
+    submenuButton.firstElementChild.src = 'images/icon-arrow-down.svg';
+}
+function openSubmenu(submenuButton, submenu){
+    submenu.style.height = 'auto';
+    submenu.dataset.submenuState = 'open';
+    submenuButton.firstElementChild.src = 'images/icon-arrow-up.svg';
+}
 
-function toggleSubmenu(target, submenu){
+function toggleSubmenu(submenuButton, submenu){
     switch(submenu.dataset.submenuState){
         case 'closed':
-            submenu.style.height = 'auto';
-            submenu.dataset.submenuState = 'open';
-            target.firstElementChild.src = 'images/icon-arrow-up.svg';
+            openSubmenu(submenuButton, submenu)
             break;
         case 'open':
-            submenu.style.height = '0';
-            submenu.dataset.submenuState = 'closed';
-            target.firstElementChild.src = 'images/icon-arrow-down.svg';
+            closeSubmenu(submenuButton, submenu)
             break;
     }
 }
@@ -45,13 +49,13 @@ document.addEventListener('click', e => {
 
 subMenus.forEach(subMenu => {
     subMenu.offsetParent.addEventListener('mouseenter', () => {
-        if(window.innerWidth >= 900){
-            toggleSubmenu(subMenu.offsetParent.firstElementChild, subMenu.offsetParent.lastElementChild);
+        if(window.innerWidth >= 900 && subMenu.dataset.submenuState == 'closed'){
+            openSubmenu(subMenu.previousElementSibling, subMenu);
         }
     })
     subMenu.offsetParent.addEventListener('mouseleave', () => {
-        if(window.innerWidth >= 900){
-            toggleSubmenu(subMenu.offsetParent.firstElementChild, subMenu.offsetParent.lastElementChild);
+        if(window.innerWidth >= 900 && subMenu.dataset.submenuState == 'open'){
+            closeSubmenu(subMenu.previousElementSibling, subMenu);
         }
     })
 })
